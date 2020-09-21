dee5a11dfb88aa952017bb86e55a4ab0c8362268
kaidornel <kai_dornel@hotmail.com>
2020-09-21 15:06:47 -0400

    added left and right joins

--- booksPractice.sql
+++ booksPractice.sql
@@ -46,8 +46,19 @@ CREATE TABLE reviews (
     REFERENCES users(user_id)
 );
 
+ALTER TABLE users
+ADD bio varchar(255);
+
+UPDATE users
+SET bio='bio one'
+WHERE user_id=1;
+
 INSERT INTO reviews (review_id, book_id, user_id, text)
 VALUES (1, 1, 1, 'good book one');
+
+INSERT INTO users (user_id, name, bio)
+VALUES (3, 'Chuck', 'bio three');
+
 
 SELECT user.name, review.text
 FROM book
@@ -59,3 +70,70 @@ SELECT users.name, reviews.text, books.title
 from users
 INNER JOIN reviews ON reviews.user_id = users.user_id
 INNER JOIN books ON reviews.book_id = books.book_id;
+
+-- inner join
+SELECT users.name, reviews.text, books.title
+from books
+INNER JOIN reviews ON reviews.book_id = books.book_id
+INNER JOIN users ON reviews.user_id = users.user_id;
+
+SELECT users.name, reviews.text
+from users
+INNER JOIN reviews ON reviews.user_id = users.user_id;
+
+SELECT * FROM reviews inner join books on reviews.book_id = books.book_id;
+
+-- left join
+SELECT country.country_name_eng, city.city_name, customer.customer_name
+FROM customer
+LEFT JOIN city ON customer.city_id = city.id
+LEFT JOIN country ON city.country_id = country.id;
+
+-- left join. this one triples everything
+SELECT users.name, users.bio, reviews.text, books.title
+FROM books
+LEFT JOIN reviews ON books.book_id = books.book_id
+LEFT JOIN users ON users.user_id = users.user_id;
+
+SELECT users.name, reviews.text  -- this repeats everything twice
+FROM users
+LEFT JOIN reviews 
+ON reviews.text = reviews.text;
+
+-- left join
+SELECT * FROM reviews Left Join users on reviews.user_id = users.user_id;
+-- left join
+SELECT * FROM users Left Join reviews on reviews.user_id = users.user_id;
+-- left join
+SELECT * FROM books Left Join reviews on reviews.book_id = books.book_id;
+
+SELECT users.name, users.bio, reviews.text
+FROM users
+LEFT JOIN reviews ON reviews.user_id = users.user_id;
+
+SELECT reviews.text, reviews.user_id, users.name
+FROM reviews
+LEFT JOIN users ON users.user_id = reviews.user_id;
+
+
+SELECT books.title, books.book_id, reviews.text, users.name
+FROM books 
+LEFT JOIN reviews on reviews.book_id = books.book_id
+LEFT JOIN users on reviews.user_id = users.user_id;
+
+-- right join
+SELECT users.name, users.bio, reviews.text
+FROM reviews
+RIGHT JOIN users ON users.user_id = reviews.user_id;
+-- right join
+SELECT users.name, users.bio, reviews.text
+FROM users
+RIGHT JOIN reviews ON reviews.user_id = users.user_id;
+-- right join
+SELECT * FROM users RIGHT JOIN reviews ON reviews.user_id = users.user_id;
+-- right join
+SELECT * FROM reviews RIGHT JOIN users ON users.user_id = reviews.user_id;
+
+SELECT reviews.review_id, reviews.text, books.title
+FROM reviews
+RIGHT JOIN books ON books.book_id = reviews.book_id;
